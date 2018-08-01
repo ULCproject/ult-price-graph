469ef9df027cb1586c5482efb2023563e2da5c05
MaherBTA <maher.bentalebali@ensi-uma.tn>
2018-08-01 16:06:36 +0100

    Setting Server & Price graph

--- server.js
+++ server.js
@@ -1,18 +1,85 @@
+const express = require('express')
+const Client = require('node-rest-client').Client
+const bodyParser = require('body-parser')
+// const client = new Client()
+
+var config = require('./config.json')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 var sqlite3 = require('sqlite3').verbose()
-var db = new sqlite3.Database(':memory:')
+var db = new sqlite3.Database('ult.db')
+let sql = `SELECT timestamp,price FROM t`
 
-db.serialize(function () {
-  db.run('CREATE TABLE lorem (info TEXT)')
-
-  var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-  for (var i = 0; i < 10; i++) {
-    stmt.run('Ipsum ' + i)
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    console.error(err)
+    return
   }
-  stmt.finalize()
+  console.log(rows)
+})
+// db.serialize(function () {
+//   // db.run('CREATE TABLE lorem (info TEXT)')
 
-  db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-    console.log(row.id + ': ' + row.info)
+//   var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+//   for (var i = 0; i < 10; i++) {
+//     stmt.run('Ipsum ' + i)
+//   }
+//   stmt.finalize()
+
+//   db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+//     console.log(row.id + ': ' + row.info)
+//   })
+// })
+
+app.get('/ult/info', function (req, res) {
+  res.send({symbol: 'ult', marketCap: '', circulatingSupply: '', maxSupply: ''})
+})
+app.get('/ult/prices', function (req, res) {
+  let prices = []
+  let sql = `SELECT timestamp,price FROM t`
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(rows)
+    prices = rows
   })
+  res.send(prices)
+})
+app.get('/ult/prices24h', function (req, res) {
+  let prices24h
+  let sql = `SELECT timestamp,price24h FROM t`
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    prices24h = rows
+  })
+  res.send(prices24h)
 })
 
-db.close()
+app.get('/ult/volume24h', function (req, res) {
+  let volumes24h
+  let sql = `SELECT timestamp,price24h FROM t`
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    volumes24h = rows
+  })
+  res.send(volumes24h)
+})
+
+app.listen(config.port, function () {
+  console.log('server started at localhost:' + config.port + ' ...')
+})
+
+// db.close()
