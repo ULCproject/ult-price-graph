932c2621105b1f3fc5b592be1dca64b46247fb7c
MaherBTA <maher.bentalebali@ensi-uma.tn>
2018-08-01 19:51:44 +0100

    server

--- server.js
+++ server.js
@@ -1,45 +1,26 @@
 const express = require('express')
-const Client = require('node-rest-client').Client
-const bodyParser = require('body-parser')
+// const Client = require('node-rest-client').Client
+// const bodyParser = require('body-parser')
 // const client = new Client()
 
 var config = require('./config.json')
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }))
+app.use(function (req, res, next) {
+  res.header('Content-Type', 'application/json')
+  next()
+})
 var sqlite3 = require('sqlite3').verbose()
 var db = new sqlite3.Database('ult.db')
-let sql = `SELECT timestamp,price FROM t`
-
-db.all(sql, [], (err, rows) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.log(rows)
-})
-// db.serialize(function () {
-//   // db.run('CREATE TABLE lorem (info TEXT)')
-
-//   var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-//   for (var i = 0; i < 10; i++) {
-//     stmt.run('Ipsum ' + i)
-//   }
-//   stmt.finalize()
-
-//   db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-//     console.log(row.id + ': ' + row.info)
-//   })
-// })
 
 app.get('/ult/info', function (req, res) {
   res.send({symbol: 'ult', marketCap: '', circulatingSupply: '', maxSupply: ''})
 })
-app.get('/ult/prices', function (req, res) {
+app.get('/ult/prices', async function (req, res) {
   let prices = []
   let sql = `SELECT timestamp,price FROM t`
   db.all(sql, [], (err, rows) => {
@@ -47,35 +28,40 @@ app.get('/ult/prices', function (req, res) {
       console.error(err)
       return
     }
-    console.log(rows)
-    prices = rows
+    rows.forEach((row) => {
+      prices.push([row['timestamp'], row['price']])
+    })
+    res.send(prices)
   })
-  res.send(prices)
 })
 app.get('/ult/prices24h', function (req, res) {
-  let prices24h
-  let sql = `SELECT timestamp,price24h FROM t`
+  let prices24h = []
+  let sql = `SELECT timestamp ,price24h  FROM t`
   db.all(sql, [], (err, rows) => {
     if (err) {
       console.error(err)
       return
     }
-    prices24h = rows
+    rows.forEach((row) => {
+      prices24h.push([row['timestamp'], row['price24h']])
+    })
+    res.send(prices24h)
   })
-  res.send(prices24h)
 })
 
-app.get('/ult/volume24h', function (req, res) {
-  let volumes24h
-  let sql = `SELECT timestamp,price24h FROM t`
+app.get('/ult/volumes24h', function (req, res) {
+  let volumes24h = []
+  let sql = `SELECT timestamp ,volume24h FROM t`
   db.all(sql, [], (err, rows) => {
     if (err) {
       console.error(err)
       return
     }
-    volumes24h = rows
+    rows.forEach((row) => {
+      volumes24h.push([row['timestamp'], row['volume24h']])
+    })
+    res.send(volumes24h)
   })
-  res.send(volumes24h)
 })
 
 app.listen(config.port, function () {
